842066e71de9a00713aa9ef1785d5525f97249c2
DINESH <dineshrautelax7@gmail.com>
2024-01-19 11:33:43 +0530

    new files

---
+++ src/components/ISAsharpener.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+ const ISAsharpener = () => {
+  return (
+  <section className='isa-section'>
+    <h2 className='black-text isa-heading'>Sharpener's Income Share Agreement (ISA)</h2>
+    <ul className='isa-ul-list'>
+      <li className='isa-listclass'>
+        <p className='isa-listclass-p1'><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" color="#bb0000" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={{color:" rgb(187, 0, 0);"}}><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path></svg></p>
+      <p className='black-text'>Pay 17% of your fixed salary for just 1 Year . There is no fixed amount and is a percentage of what you earn. This keeps us motivated and we will always try to get you the best offer you deserve.</p>
+      </li>
+      <li className='isa-listclass'>
+        <p className='isa-listclass-p1'><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" color="#bb0000" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={{color:" rgb(187, 0, 0);"}}><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path></svg></p>
+      <p className='black-text'>ISA pauses if you lose your job due to reasons beyond your control.</p>
+      </li>
+      <li className='isa-listclass'>
+        <p className='isa-listclass-p1'><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" color="#bb0000" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={{color:" rgb(187, 0, 0);"}}><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path></svg></p>
+      <p className='black-text'>ISA starts after you have joined the company and started earning.</p>
+      </li>
+      <li className='isa-listclass'>
+        <p className='isa-listclass-p1'><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" color="#bb0000" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={{color:" rgb(187, 0, 0);"}}><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path></svg></p>
+      <p className='black-text'>If you wish to discontinue from the program, or we notice that you have stopped working, ISA will be terminated. Based on the amount of time you have spent on the platform, you will be charged a termination fee.</p>
+      </li>
+      <li className='isa-listclass'>
+        <p className='isa-listclass-p1'><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" color="#bb0000" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={{color:" rgb(187, 0, 0);"}}><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"></path></svg></p>
+      <p className='black-text'>ISA is valid for just 1 year after signing. If you don’t get a job within a year despite completing your course, ISA is deemed null and void.</p>
+      </li>
+      
+    </ul>
+  </section>
+  )
+}
+export  default ISAsharpener;
